5f473c243f952d6a5944b4696a37bc203c979be2
g-tonin <158225343+g-tonin@users.noreply.github.com>
2024-02-15 08:34:36 -0300

    0.0.2

--- Dices.cs
+++ Dices.cs
@@ -10,17 +10,16 @@ class Dices
     public int d4;
     public int d6;
     public int d8;
-    public int d10;
     public int d12;
     public int d20;
 
-    Random numberGen = new Random();
-
-    d4 = numberGen.Next(1,5);
-    d6 = numberGen.Next(1,7);
-    d8 = numberGen.Next(1,9);
-    d10 = numberGen.Next(1,11);
-    d12 = numberGen.Next(1,13);
-    d20 = numberGen.Next(1,21);
-
+    public Dices()
+    {
+        Random numberGen = new Random();
+        d4 = numberGen.Next(1, 5);
+        d6 = numberGen.Next(1, 7);
+        d8 = numberGen.Next(1, 9);
+        d12 = numberGen.Next(1, 13);
+        d20 = numberGen.Next(1, 21);
+    }
 }
