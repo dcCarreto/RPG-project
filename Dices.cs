12cf6aef1436da77b01cc71a010cdea3be7a37f6
g-tonin <158225343+g-tonin@users.noreply.github.com>
2024-02-15 12:24:01 -0300

    dices.0.0.4

--- Dices.cs
+++ Dices.cs
@@ -1,20 +1,26 @@
 class Dices
 {
-    public int d4;
-    public int d6;
-    public int d8;
-    public int d12;
-    public int d10;
-    public int d20;
+    public int d4 = 4;
+    public int d6 = 6;
+    public int d8 = 8;
+    public int d10 = 10;
+    public int d12 = 12;
+    public int d20 = 20;
 
-    public Dices()
+    public int Roll(int dice)
     {
         Random numberGen = new Random();
-        d4 = numberGen.Next(1, 5);
-        d6 = numberGen.Next(1, 7);
-        d8 = numberGen.Next(1, 9);
-        d10 = numberGen.Next(1, 11);
-        d12 = numberGen.Next(1, 13);
-        d20 = numberGen.Next(1, 21);
+        int result = numberGen.Next(1, dice + 1);
+        return result;
     }
 }
+
+// 0.0.4
+// Na versão anterior (0.0.3), o numberGen estava sendo executado diretamente no atributo da classe.
+// Como consequência, numberGen só era executado na criação de uma instância. isso tornava, para as futuras chamadas da instância, os valores dos atributos em valores fixos.
+// Para gerar um novo valor, seria necessário criar uma nova instância.
+// Para solucionar esse problema, criei a função Roll().
+// --
+// Exemplo de uso:
+// Dices dice = new Dices();
+// dice.Roll(dice.d4);
